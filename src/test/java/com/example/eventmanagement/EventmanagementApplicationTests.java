2dd60c5d6abcdfba5e5aa3d6493bdef6374c495f
Harsh Vardhan <harsh.vardhan02@nagarro.com>
2026-01-24 14:33:54 +0530

    Code refactor

--- src/test/java/com/example/eventmanagement/EventmanagementApplicationTests.java
+++ src/test/java/com/example/eventmanagement/EventmanagementApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.eventmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+//@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class EventmanagementApplicationTests {
 
 	@Test
